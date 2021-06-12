5739b2ee75bdf3ad1fe5c0b5fdf8fdd3e3fc8e1f
Antonio Cavallo <a.cavallo@cavallinux.eu>
2021-06-12 15:45:36 -0400

    fix version/hash on beta builds

--- setup.py
+++ setup.py
@@ -6,9 +6,22 @@ from setuptools import setup, find_namespace_packages
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
 import click.plus
 
+version = click.plus.__version__
+if os.getenv("MYVERSION"):
+    assert os.getenv("MYHASH")
+    version = os.getenv("MYVERSION")
+    thehash = os.getenv("MYHASH")
+    with open(click.plus.__file__, "w") as fp:
+    fp.write(f"""
+__version__ = "{version}"
+__hash__ = "{thehash}"
+""".strip())
+
+
+
 setup(
     name="click-plus",
-    version=click.plus.__version__,
+    version=version,
     url="https://github.com/cav71/click-plus",
     #packages=find_namespace_packages(where="src"),
     packages=["click.plus",],
