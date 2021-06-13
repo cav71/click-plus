d79a2c4be13977a2a773728a310364172dfd1285
Antonio Cavallo <a.cavallo@cavallinux.eu>
2021-06-12 20:30:22 -0400

    cleanups

--- setup.py
+++ setup.py
@@ -9,12 +9,29 @@ from setuptools import setup, find_namespace_packages
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
 import click.plus
 
-version = click.plus.__version__
 
+def hubversion(gdata):
+    txt = gdata["ref"]
+    number = gdata['run_number']
+    shasum = gdata["sha"]
+    head, _, rest = txt.partition("/")
+
+    cases = [
+        ("refs/heads/master", click.plus.__version__),
+        ("refs/heads/beta/", f"b{number}"),
+        ("refs/tags/release/", ""),
+    ]
+    for pat, out in cases:
+        if not txt.startswith(pat):
+            continue
+        return txt[len(pat):] + out, shasum
+    raise RuntimeError("unhandled github ref", txt)
+
+
+version = click.plus.__version__
 if os.getenv("GITHUB_DUMP"):
     gdata = json.loads(os.getenv("GITHUB_DUMP"))   
-    version = gdata["ref"].rpartition("/")[2] + f"b{gdata['run_number']}"
-    thehash = gdata["sha"]
+    version, thehash = hubversion(gdata)
     with open(click.plus.__file__, "w") as fp:
         fp.write(f"""
 __version__ = "{version}"
