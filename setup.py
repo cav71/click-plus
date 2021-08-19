b5334c351570094b4bad8cc09019e7a76fc8ef11
antonio <a.cavallo@cavallinux.eu>
2021-08-19 12:39:01 -0400

    overhaul project structure

--- setup.py
+++ setup.py
@@ -10,14 +10,34 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "src
 import click.plus
 
 
-def hubversion(gdata):
+def hubversion(gdata, fallback):
+    """returns (version, shasum)
+    >>> hubversion({
+        'ref': 'refs/heads/beta/0.0.4',
+        'sha': '2169f90c22e',
+        'run_number': '8',
+    }, None)
+    ('0.0.4b8', '2169f90c22e')
+    >>> hubversion({
+        'ref': 'refs/tags/release/0.0.3',
+        'sha': '5547365c82',
+        'run_number': '3',
+    }, None)
+    ('0.0.3', '5547365c82')
+    >>> hubversion({
+        'ref': 'refs/heads/master',
+        'sha': '2169f90c',
+        'run_number': '20',
+    }, '123'))
+    ('123', '2169f90c')
+"""
     txt = gdata["ref"]
     number = gdata['run_number']
     shasum = gdata["sha"]
     head, _, rest = txt.partition("/")
 
     cases = [
-        ("refs/heads/master", click.plus.__version__),
+        ("refs/heads/master", fallback,),
         ("refs/heads/beta/", f"b{number}"),
         ("refs/tags/release/", ""),
     ]
@@ -28,16 +48,34 @@ def hubversion(gdata):
     raise RuntimeError("unhandled github ref", txt)
 
 
-version = click.plus.__version__
-if os.getenv("GITHUB_DUMP"):
-    gdata = json.loads(os.getenv("GITHUB_DUMP"))   
-    version, thehash = hubversion(gdata)
-    with open(click.plus.__file__, "w") as fp:
-        fp.write(f"""
-__version__ = "{version}"
-__hash__ = "{thehash}"
-""".strip())
+def update_version(data, path, fallback):
+    if not data:
+        return
 
+    gdata = json.loads(data)
+    version, thehash = hubversion(gdata, fallback)
+
+    lines = pathlib.Path(path).read_text().split("\n")
+
+    exp = re.compile(r"__version__\s*=\s*")
+    exp1 = re.compile(r"__hash__\s*=\s*")
+    assert len([ l for l in lines if exp.search(l)]) == 1
+    assert len([ l for l in lines if exp1.search(l)]) == 1
+
+    lines = [
+        f"__version__ = \"{version}\"" if exp.search(l) else
+        f"__hash__ = \"{thehash}\"" if exp1.search(l) else
+        l
+        for l in lines
+    ]
+
+    pathlib.Path(mono2repo.__file__).write_text("\n".join(lines))
+    return version
+
+
+version = update_version(os.getenv("GITHUB_DUMP"),
+    click,plus.__file__,
+    click,plus.__version__)
 
 packages = find_namespace_packages(where="src")
 packages.remove("click")
