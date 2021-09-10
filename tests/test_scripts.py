60a18ae2133bce75e807ba0ef55b509cdc5a0e2f
antonio <a.cavallo@cavallinux.eu>
2021-09-10 08:37:01 -0400

    use DATADIR

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,23 +1,37 @@
+DATADIR = "examples"
+
+
 def test_script0(scripter):
     "test a simple script"
 
     script = scripter / "script-0.py"
-    
-    # test help
-    result = script.run(["--help", ])
 
-    assert """
+    # test help
+    result = script.run(
+        [
+            "--help",
+        ]
+    )
+
+    assert (
+        """
 Usage: script-0.py [OPTIONS] VALUE
 
 Options:
   --boost INTEGER
   --help           Show this message and exit.
-""".lstrip() == result.out
+""".lstrip()
+        == result.out
+    )
 
     # test logic: factor * value * boost
 
     # 2 * 13 * 1
-    result = script.run(["13",])
+    result = script.run(
+        [
+            "13",
+        ]
+    )
     assert "Got 26" == result.out.strip()
 
     # 2 * 13 * 3
@@ -31,24 +45,28 @@ def test_script1(scripter):
     script = scripter / "script-1.py"
 
     # test help
-    assert not script.run(["--help", ]).code
+    assert not script.run(
+        [
+            "--help",
+        ]
+    ).code
 
     # 2 * 13 * 1
-    result = script.run([ "factor-2", "13" ])
+    result = script.run(["factor-2", "13"])
     assert "Got 26" == result.out.strip()
 
     # 2 * 13 * 3
-    result = script.run([ "factor-2", "13", "--boost", "3"])
+    result = script.run(["factor-2", "13", "--boost", "3"])
     assert "Got 78" == result.out.strip()
 
     # 10 * 13 * 1
-    result = script.run([ "factor-10", "13" ])
+    result = script.run(["factor-10", "13"])
     assert "Got 130" == result.out.strip()
 
     # 10 * 13 * 3
-    result = script.run([ "factor-10", "13", "--boost", "3"])
+    result = script.run(["factor-10", "13", "--boost", "3"])
     assert "Got 390" == result.out.strip()
-    
+
 
 def test_script2(scripter):
     "test a script with group"
@@ -56,9 +74,13 @@ def test_script2(scripter):
     script = scripter / "script-2.py"
 
     # test help
-    assert not script.run(["--help", ]).code
+    assert not script.run(
+        [
+            "--help",
+        ]
+    ).code
 
-    result = script.run([ "99" ])
+    result = script.run(["99"])
     assert "Got 99" == result.out.strip()
     assert "This is the ExtensionBase.setup" in result.err
     assert "This is the ExtensionBase.process" in result.err
