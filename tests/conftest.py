0e6971691cb79049514470bbcbe2b2713050eff7
antonio <a.cavallo@cavallinux.eu>
2021-09-10 08:35:53 -0400

    pytest conftest.py update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,37 +1,70 @@
+import os
 import sys
 import pathlib
-import inspect
+import contextlib
 import collections
 import subprocess
 import pytest
 
 
 @pytest.fixture()
-def scripter(tmp_path_factory):
-    R = collections.namedtuple("R", "out,err,code")
-    class E:
-        def __init__(self, script, tmpdir, exe=sys.executable):
+def datadir(request):
+    basedir = pathlib.Path(__file__).parent / "data"
+    if os.getenv("DATADIR"):
+        basedir = pathlib.Path(os.getenv("DATADIR"))
+    basedir = basedir / getattr(request.module, "DATADIR", "")
+    return basedir
+
+
+@pytest.fixture()
+def scripter(tmp_path_factory, datadir):
+    """handles script (cli) execution
+
+    def test(scripter):
+        script = scripter / "script-file.py"
+        result = script.run(["--help"]) # this will execute:
+                                        #   script-file.py --help
+        assert result.out and result.err
+    """
+    Result = collections.namedtuple("R", "out,err,code")
+
+    class Exe:
+        def __init__(self, script, workdir, exe=sys.executable):
             self.script = script
-            self.tmpdir = tmpdir
+            self.workdir = workdir
             self.exe = exe
+            if not pathlib.Path(script).exists():
+                raise RuntimeError(f"script file {script} found")
 
         def run(self, args, cwd=None):
-            cmd = [ str(a) for a in [self.exe, self.script, *args] ]
-            self.p = subprocess.Popen(cmd,
-                        cwd=self.tmpdir if cwd is True else cwd,
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            out, err = self.p.communicate()
-            out = out if isinstance(out, str) else str(out, encoding="utf-8")
-            err = err if isinstance(err, str) else str(err, encoding="utf-8")
+            cmd = [str(a) for a in [self.exe, self.script, *args]]
 
-            return R(out.replace("\r\n", "\n"), err.replace("\r\n", "\n"), self.p.returncode)
+            with contextlib.ExitStack() as stack:
+                fpout = stack.enter_context((self.workdir / "stdout.txt").open("w"))
+                fperr = stack.enter_context((self.workdir / "stderr.txt").open("w"))
+                self.p = subprocess.Popen(
+                    cmd,
+                    cwd=self.workdir if cwd is True else cwd,
+                    stdout=fpout,
+                    stderr=fperr,
+                )
+                self.p.communicate()
+            out = (self.workdir / "stdout.txt").read_text()
+            err = (self.workdir / "stderr.txt").read_text()
+            return Result(
+                out.replace("\r\n", "\n"), err.replace("\r\n", "\n"), self.p.returncode
+            )
+
+        def compare(self, datadir):
+            pass
 
     class Scripter:
-        def __init__(self):
-            self.src = pathlib.Path(__file__).resolve().parent / "data"
+        def __init__(self, src):
+            self.src = src
             self.exe = sys.executable
+
         def __truediv__(self, path):
             tmpdir = tmp_path_factory.mktemp(pathlib.Path(path).with_suffix("").name)
-            return E(self.src / path, tmpdir)
+            return Exe(self.src / path, tmpdir)
 
-    return Scripter()
+    return Scripter(datadir)
