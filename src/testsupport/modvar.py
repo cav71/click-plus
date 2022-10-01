5f0a89409c21f7d3b0ed2d47deaaa32c90670e5d
antonio <a.cavallo@cavallinux.eu>
2022-09-30 21:00:01 -0400

    added modvar plugin

---
+++ src/testsupport/modvar.py
@@ -0,0 +1,54 @@
+"""modvar pytest fixture to store and restore variables
+
+modvar is a pytest fixture helping with the task of preserving variables
+in a test.
+
+How to use it:
+
+def testme(modvar):
+    with modvar(os.environ):
+        os.environ["HELLO"] = "World"
+        -> so the environ variable stores "World"
+    -> here HELLO is deleted (if didn't exist)
+"""
+from typing import Dict, Any
+
+import pytest
+
+
+class VarManager:
+    class NA:  # pylint: disable=too-few-public-methods
+        pass
+
+    def __init__(self, source: Dict[str, Any], val=None, /, **kwars):
+        self.source = source
+        self.mods = dict(val or {})
+        self.mods.update(kwars)
+        self.values : dict[Any, Any] = {}
+
+    def __enter__(self):
+        for name, value in self.mods.items():
+            self.set(name, value)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for name, value in self.values.items():
+            self.set(name, value)
+
+    def set(self, name, value):
+        self.values[name] = self.source.get(name, self.NA)
+        if value is not self.NA:
+            self.source[name] = value
+        elif name in self.source:
+            del self.source[name]
+        return self.values[name]
+
+    def delete(self, name):
+        if name not in self.source:
+            self.values[name] = self.NA
+            return self.NA
+        return self.set(name, self.NA)
+
+@pytest.fixture(scope="function")
+def modvar():
+    yield VarManager
---
+++ tests/testsupport/test_modvar.py
@@ -0,0 +1,47 @@
+"""Test the pytest modvar plugin
+"""
+
+import pytest
+
+MYVAR = "xyz"
+
+
+def test_modvar(modvar):
+    "test modvar usage"
+
+    assert MYVAR == "xyz"
+    with pytest.raises(NameError):
+        # NEWVAR doesn't exist
+        NEWVAR  # pylint: disable=undefined-variable,pointless-statement
+
+    with modvar(globals(), {"MYVAR": 123}) as mod:
+        assert MYVAR == 123
+        # injecting NEWVAR into the global namespace
+        mod.set("NEWVAR", [4, 5, 6])
+        assert NEWVAR == [4, 5, 6]  # pylint: disable=undefined-variable
+
+    # restoring the original values
+    assert MYVAR == "xyz"
+    with pytest.raises(NameError):
+        NEWVAR  # pylint: disable=undefined-variable,pointless-statement
+
+
+def test_modvar_operations(modvar):
+    "test modvar operations"
+
+    # starting point
+    assert MYVAR == "xyz"
+    with pytest.raises(NameError):
+        NEWVAR  # pylint: disable=undefined-variable,pointless-statement
+
+    with modvar(globals()) as mod:
+        mod.delete("MYVAR")
+        with pytest.raises(NameError):
+            MYVAR  # pylint: disable=undefined-variable,pointless-statement
+
+        mod.set("NEWVAR", 12)
+        assert NEWVAR == 12  # pylint: disable=undefined-variable
+
+    assert MYVAR == "xyz"
+    with pytest.raises(NameError):
+        NEWVAR  # pylint: disable=undefined-variable,pointless-statement
