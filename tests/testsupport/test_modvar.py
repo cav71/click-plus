8f48a19d5e490baa598ba29e4b84132048dd62c0
antonio <a.cavallo@cavallinux.eu>
2022-10-01 10:19:46 -0400

    udpates

--- tests/testsupport/test_modvar.py
+++ tests/testsupport/test_modvar.py
@@ -12,18 +12,18 @@ def test_modvar(modvar):
     assert MYVAR == "xyz"
     with pytest.raises(NameError):
         # NEWVAR doesn't exist
-        NEWVAR  # pylint: disable=undefined-variable,pointless-statement
+        NEWVAR  # type: ignore # noqa
 
     with modvar(globals(), MYVAR=123) as mod:
         assert MYVAR == 123
         # injecting NEWVAR into the global namespace
         mod.set("NEWVAR", [4, 5, 6])
-        assert NEWVAR == [4, 5, 6]  # pylint: disable=undefined-variable
+        assert NEWVAR == [4, 5, 6]  # type: ignore # noqa
 
     # restoring the original values
     assert MYVAR == "xyz"
     with pytest.raises(NameError):
-        NEWVAR  # pylint: disable=undefined-variable,pointless-statement
+        NEWVAR  # type: ignore # noqa
 
 
 def test_modvar_operations(modvar):
@@ -32,16 +32,24 @@ def test_modvar_operations(modvar):
     # starting point
     assert MYVAR == "xyz"
     with pytest.raises(NameError):
-        NEWVAR  # pylint: disable=undefined-variable,pointless-statement
+        NEWVAR  # type: ignore # noqa
 
     with modvar(globals()) as mod:
         mod.delete("MYVAR")
         with pytest.raises(NameError):
-            MYVAR  # pylint: disable=undefined-variable,pointless-statement
+            MYVAR  # type: ignore # noqa
 
         mod.set("NEWVAR", 12)
-        assert NEWVAR == 12  # pylint: disable=undefined-variable
+        assert NEWVAR == 12  # type: ignore # noqa
 
     assert MYVAR == "xyz"
     with pytest.raises(NameError):
-        NEWVAR  # pylint: disable=undefined-variable,pointless-statement
+        NEWVAR  # type: ignore # noqa
+
+
+def test_modvar_shadow_myvar(modvar):
+    assert MYVAR == "xyz"
+    with modvar(globals(), MYVAR=modvar.NA):
+        with pytest.raises(NameError):
+            MYVAR  # pylint: disable=undefined-variable,pointless-statement
+    assert MYVAR == "xyz"
