aac011e7f624263df6061c0fd641147e5b346431
antonio <a.cavallo@cavallinux.eu>
2022-09-30 21:45:54 -0400

    udpates

---
+++ src/testsupport/datadir.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+from typing import Optional
+import pytest
+
+
+class MyPath(Path):
+    def makedirs(self):
+        return self.mkdir(parents=True, exist_ok=True)
+
+
+def npath(path: Optional[Path]=None) -> MyPath:
+    return MyPath(os.path.normpath(Path(path or Path.cwd()).expanduser()))
+
+
+@pytest.fixture(scope="function")
+def datadir(request) -> Path:
+    basedir = request.config.getoption("datadir")
+    return npath(basedir / getattr(request.module, "DATADIR", ""))
+
+
+@pytest.fixture(scope="function")
+def testdir(datadir, request) -> Path:
+    from conftest import __file__
+    result = (
+        Path(request.module.__file__)
+        .relative_to(Path(__file__).parent)
+        .with_suffix("")
+    )
+    return npath(datadir / result)
+
+
+def pytest_addoption(parser):
+    from conftest import __file__
+    group = parser.getgroup("datadir")
+    group.addoption(
+        "--datadir",
+        default=os.getenv("DATADIR", npath(Path(__file__).parent / "datadir")),
+        type=Path,
+        help="""
+        source data: the datadir fixture will be a path rooted under DATADIR. resolution order is:
+
+          1. path relative to the root conftest.py
+          2. environment variable DATADIR
+          3. command line argumnet --datadir
+    
+        additionally if a test module contains a DATADIR global variable it 
+        will be appended to the datadir fixture
+    """
+    )
---
+++ tests/testsupport/test_datadir.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import pytest
+
+PROJECTDIR = Path(__file__).parent.parent.parent
+
+
+@pytest.fixture(scope="function")
+def fixer(modvar):
+    with modvar(globals(), {"DATADIR": "abc"}) as mod:
+        yield mod
+
+
+@pytest.fixture(scope="function")
+def absolute_fixer(modvar):
+    with modvar(globals(), {"DATADIR": "/abc/def"}) as mod:
+        yield mod
+
+
+def test_datadir(datadir, testdir):
+    assert all(isinstance(d, Path) for d in (datadir, testdir))
+    assert datadir == (PROJECTDIR / "tests" / "datadir")
+    assert testdir == (
+        PROJECTDIR / "tests" / "datadir" / "testsupport" / "test_datadir"
+    )
+
+
+def test_datadir_with_datadir(fixer, datadir, testdir):
+    assert all(isinstance(d, Path) for d in (datadir, testdir))
+    assert datadir == (PROJECTDIR / "tests" / "datadir" / "abc")
+    assert testdir == (
+        PROJECTDIR / "tests" / "datadir" / "abc" / "testsupport" / "test_datadir"
+    )
+
+
+def test_datadir_with_absolute_datadir(absolute_fixer, datadir, testdir):
+    assert all(isinstance(d, Path) for d in (datadir, testdir))
+    assert datadir == Path("/abc/def")
+    assert testdir == Path("/abc/def/testsupport/test_datadir")
